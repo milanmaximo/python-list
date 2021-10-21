2b98f208bf4a49ca4ab74ed80ec143e885b99c60
Milan <milanleon@protonmail.com>
2021-10-22 00:01:13 +0200

    Add python list

--- list.py
+++ list.py
@@ -1,15 +1,15 @@
 import os
-list1 = [8, 9, 3, 6, 1, 10]
-list1.reverse()
+List1 = [8, 9, 3, 6, 1, 10]
+List1.reverse()
 print("The reversed list is", List1)
 
-list2 = [91, 67, 120, 34, 76, 54, 78, 87, 65, 64, 365]
-list2.sort()
+List2 = [91, 67, 120, 34, 76, 54, 78, 87, 65, 64, 365]
+List2.sort()
 print("The sorted list is", List2)
 
-list3 = []
-list3 = list.copy()
+List3 = []
+List3 = List1.copy()
 print("List3 =", List3)
 
-indexvalue = list2[2:6]
+indexvalue = List2[2:6]
 print("index value are", indexvalue)
